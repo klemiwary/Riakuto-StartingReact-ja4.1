c19ba54c7bb227680aadff885a66939d0de33afe
Yuka O'oka <homeyuka@gmail.com>
2024-03-16 09:30:45 +0900

    add 6-4-4 svelte sample

---
+++ 06-history/04-gen3/svelte-sample/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  let count = 0;
+  let name = 'Yukina';
+  $: numName = `Hello * ${count}`;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<h1>{numName}</h1>
+<input type="text" bind:value={name} />
+<button on:click={handleClick}>Greet</button>
+{#each [...Array(count)]as _}
+<p class="greet">
+  Hello, {name}!
+</p>
+{/each}
+
+<style>
+  .greet {
+    font-weight: bold;
+    margin: 0.5rem 1rem;
+  }
+</style>
